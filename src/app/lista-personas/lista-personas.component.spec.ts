f455ec2f70dc74c937ab4357ff6a27e7ed204107
Aylenhel1 <aylen.helbert@cloversistemas.com.ar>
2022-07-18 12:59:48 -0300

    1 file modified, 4 added

---
+++ src/app/lista-personas/lista-personas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaPersonasComponent } from './lista-personas.component';
+
+describe('ListaPersonasComponent', () => {
+  let component: ListaPersonasComponent;
+  let fixture: ComponentFixture<ListaPersonasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaPersonasComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaPersonasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/lista-personas/lista-personas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-personas',
+  templateUrl: './lista-personas.component.html',
+  styleUrls: ['./lista-personas.component.css']
+})
+export class ListaPersonasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
